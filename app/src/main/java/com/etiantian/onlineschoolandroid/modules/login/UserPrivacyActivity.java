5a14491213a01ec170a610b7e2666f25c08d551e
waitwalker <waitwalker@163.com>
2020-09-04 16:59:00 +0800

    用户协议中webview使用

--- app/src/main/java/com/etiantian/onlineschoolandroid/modules/login/UserPrivacyActivity.java
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/login/UserPrivacyActivity.java
@@ -4,29 +4,92 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.etiantian.onlineschoolandroid.R;
 import com.etiantian.onlineschoolandroid.entrance.BaseActivity;
+import com.kaopiz.kprogresshud.KProgressHUD;
 
 public class UserPrivacyActivity extends BaseActivity {
+
+    private WebView webView;
+    private KProgressHUD hud;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_privacy);
 
+        hud = KProgressHUD.create(this)
+                .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE);
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
-            actionBar.setTitle("用户隐私政策");
+            actionBar.setTitle("用户协议");
             actionBar.setDisplayShowTitleEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setElevation(5);
             actionBar.show();
         }
+
+        webView = findViewById(R.id.webview);
+        webView.loadUrl("https://www.etiantian.com/about/mobile/servandpriv.html");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);// 允许使用js
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setSupportZoom(false);
+        webSettings.setSupportMultipleWindows(false);
+        webView.setWebChromeClient(webChromeClient);
+        webView.setWebViewClient(webViewClient);
     }
+
+    private WebViewClient webViewClient = new WebViewClient(){
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            hud.show();
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            hud.dismiss();
+            super.onPageFinished(view, url);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+    };
+
+    private WebChromeClient webChromeClient = new WebChromeClient(){
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            return super.onJsAlert(view, url, message, result);
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+        }
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            Log.d("1","用户协议网页加载进度:" + newProgress);
+            super.onProgressChanged(view, newProgress);
+        }
+    };
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
