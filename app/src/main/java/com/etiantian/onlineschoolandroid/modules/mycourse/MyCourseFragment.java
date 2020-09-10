3e88e6e5efa63bdb1c083c6101d0ed4932b7b7d0
waitwalker <waitwalker@163.com>
2020-09-10 16:28:23 +0800

    联通活动课添加

--- app/src/main/java/com/etiantian/onlineschoolandroid/modules/mycourse/MyCourseFragment.java
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/mycourse/MyCourseFragment.java
@@ -2,8 +2,6 @@ package com.etiantian.onlineschoolandroid.modules.mycourse;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +14,16 @@ import com.etiantian.onlineschoolandroid.R;
 import com.etiantian.onlineschoolandroid.api.NetworkManager;
 import com.etiantian.onlineschoolandroid.base.BaseFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyCourseFragment extends BaseFragment implements CompoundButton.OnClickListener {
 
     private View root;
     private ViewGroup course_menu_container;
     private Button course_menu_button;
-    private MyCourseGridView gridView;
+    private MyCourseGridView zhiLingGridView;
+    private MyCourseGridView zhiXueGridView;
 
     public MyCourseFragment() {
         // Required empty public constructor
@@ -64,9 +66,20 @@ public class MyCourseFragment extends BaseFragment implements CompoundButton.OnC
             public void onSuccess(Object responseObj) {
 
                 MyCourseSubjectModel courseSubjectModel = (MyCourseSubjectModel) responseObj;
-                gridView = root.findViewById(R.id.my_course_grid);
-                MyCourseGridViewAdapter adapter = new MyCourseGridViewAdapter(getContext(),courseSubjectModel.getData());
-                gridView.setAdapter(adapter);
+                zhiLingGridView = root.findViewById(R.id.my_course_zhiling_grid);
+                List<MyCourseSubjectModel.DataBean> zhiLingList = new ArrayList<>();
+                List<MyCourseSubjectModel.DataBean> zhiXueList = new ArrayList<>();
+                for (int i = 0; i < courseSubjectModel.getData().size(); i++) {
+                    MyCourseSubjectModel.DataBean dataBean = courseSubjectModel.getData().get(i);
+                    if (dataBean.getSubjectId() == 2 || dataBean.getSubjectId() == 3 || dataBean.getSubjectId() == 4 || dataBean.getSubjectId() == 5) {
+                        zhiLingList.add(dataBean);
+                    } else {
+                        zhiXueList.add(dataBean);
+                    }
+                }
+
+                MyCourseGridViewAdapter adapter = new MyCourseGridViewAdapter(getContext(), zhiLingList);
+                zhiLingGridView.setAdapter(adapter);
                 Log.d("1","获取数据成功");
             }
 
