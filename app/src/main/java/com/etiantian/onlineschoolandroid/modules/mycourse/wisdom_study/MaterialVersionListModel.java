3d1a90bf2504669f8f4b1cdd515eca6e6c1b498c
waitwalker <waitwalker@163.com>
2020-09-17 09:46:46 +0800

    获取教材版本列表model添加

---
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/mycourse/wisdom_study/MaterialVersionListModel.java
@@ -0,0 +1,227 @@
+package com.etiantian.onlineschoolandroid.modules.mycourse.wisdom_study;
+
+import java.util.List;
+
+///
+/// @description 教材版本列表
+/// @author waitwalker
+/// @time 2020/9/17 9:46 AM
+///
+public class MaterialVersionListModel {
+
+    /**
+     * code : 1
+     * msg : 操作成功
+     * data : [{"versionId":53450,"versionName":"人教社课程标准实验教材","abbreviation":"人教版","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51511,"materialName":"初一上册","versionId":53450,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51311,"materialName":"初一下册","versionId":53450,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53381,"versionName":"北京师范大学课程标准实验教材","abbreviation":"北师大版","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51348,"materialName":"初一上册","versionId":53381,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51355,"materialName":"初一下册","versionId":53381,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53387,"versionName":"华东师范大学课程标准实验教材","abbreviation":"华东师大版","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51550,"materialName":"初一上册","versionId":53387,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51549,"materialName":"初一下册","versionId":53387,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53389,"versionName":"浙江教育出版社教材","abbreviation":"浙教版","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51670,"materialName":"初一上册","versionId":53389,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51671,"materialName":"初一下册","versionId":53389,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53398,"versionName":"人教社五四制教材","abbreviation":"人教五四制","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":168860,"materialName":"小学六年级上册","versionId":53398,"versionName":null,"ifFiveFour":1,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51716,"materialName":"初一上册","versionId":53398,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":168861,"materialName":"小学六年级下册","versionId":53398,"versionName":null,"ifFiveFour":1,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51738,"materialName":"初一下册","versionId":53398,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53403,"versionName":"河北教育出版社教材","abbreviation":"冀教版","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51675,"materialName":"初一上册","versionId":53403,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51676,"materialName":"初一下册","versionId":53403,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53407,"versionName":"江苏科技出版社","abbreviation":"苏科版","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51368,"materialName":"初一上册","versionId":53407,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51369,"materialName":"初一下册","versionId":53407,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53410,"versionName":"湖南教育出版社","abbreviation":"湘教版","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51695,"materialName":"初一上册","versionId":53410,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51712,"materialName":"初一下册","versionId":53410,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53413,"versionName":"上海科学技术出版社","abbreviation":"沪科版","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51720,"materialName":"初一上册","versionId":53413,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51734,"materialName":"初一下册","versionId":53413,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53424,"versionName":"北京出版社 北京教育出版社","abbreviation":"北京版","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51362,"materialName":"初一上册","versionId":53424,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51363,"materialName":"初一下册","versionId":53424,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":53432,"versionName":"山东教育出版社（五四制）","abbreviation":"鲁教五四制","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":91340,"materialName":"小学六年级上册","versionId":53432,"versionName":null,"ifFiveFour":1,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51708,"materialName":"初一上册","versionId":53432,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":91341,"materialName":"小学六年级下册","versionId":53432,"versionName":null,"ifFiveFour":1,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51737,"materialName":"初一下册","versionId":53432,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]},{"versionId":184670,"versionName":"上海教育出版社（新）","abbreviation":"沪教版（新）","gradeId":null,"subjectId":2,"teachingMaterialDTOList":[{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":184672,"materialName":"小学六年级下册","versionId":184670,"versionName":null,"ifFiveFour":1,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":184673,"materialName":"初一上册","versionId":184670,"versionName":null,"ifFiveFour":1,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":184674,"materialName":"初一下册","versionId":184670,"versionName":null,"ifFiveFour":1,"materialPosterImg":null,"useCount":null,"orderIndex":null}]}]
+     */
+
+    private int code;
+    private String msg;
+    private List<DataBean> data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * versionId : 53450
+         * versionName : 人教社课程标准实验教材
+         * abbreviation : 人教版
+         * gradeId : null
+         * subjectId : 2
+         * teachingMaterialDTOList : [{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51511,"materialName":"初一上册","versionId":53450,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null},{"abbreviation":null,"gradeId":6,"subjectId":2,"materialId":51311,"materialName":"初一下册","versionId":53450,"versionName":null,"ifFiveFour":0,"materialPosterImg":null,"useCount":null,"orderIndex":null}]
+         */
+
+        private int versionId;
+        private String versionName;
+        private String abbreviation;
+        private Object gradeId;
+        private int subjectId;
+        private List<TeachingMaterialDTOListBean> teachingMaterialDTOList;
+
+        public int getVersionId() {
+            return versionId;
+        }
+
+        public void setVersionId(int versionId) {
+            this.versionId = versionId;
+        }
+
+        public String getVersionName() {
+            return versionName;
+        }
+
+        public void setVersionName(String versionName) {
+            this.versionName = versionName;
+        }
+
+        public String getAbbreviation() {
+            return abbreviation;
+        }
+
+        public void setAbbreviation(String abbreviation) {
+            this.abbreviation = abbreviation;
+        }
+
+        public Object getGradeId() {
+            return gradeId;
+        }
+
+        public void setGradeId(Object gradeId) {
+            this.gradeId = gradeId;
+        }
+
+        public int getSubjectId() {
+            return subjectId;
+        }
+
+        public void setSubjectId(int subjectId) {
+            this.subjectId = subjectId;
+        }
+
+        public List<TeachingMaterialDTOListBean> getTeachingMaterialDTOList() {
+            return teachingMaterialDTOList;
+        }
+
+        public void setTeachingMaterialDTOList(List<TeachingMaterialDTOListBean> teachingMaterialDTOList) {
+            this.teachingMaterialDTOList = teachingMaterialDTOList;
+        }
+
+        public static class TeachingMaterialDTOListBean {
+            /**
+             * abbreviation : null
+             * gradeId : 6
+             * subjectId : 2
+             * materialId : 51511
+             * materialName : 初一上册
+             * versionId : 53450
+             * versionName : null
+             * ifFiveFour : 0
+             * materialPosterImg : null
+             * useCount : null
+             * orderIndex : null
+             */
+
+            private Object abbreviation;
+            private int gradeId;
+            private int subjectId;
+            private int materialId;
+            private String materialName;
+            private int versionId;
+            private Object versionName;
+            private int ifFiveFour;
+            private Object materialPosterImg;
+            private Object useCount;
+            private Object orderIndex;
+
+            public Object getAbbreviation() {
+                return abbreviation;
+            }
+
+            public void setAbbreviation(Object abbreviation) {
+                this.abbreviation = abbreviation;
+            }
+
+            public int getGradeId() {
+                return gradeId;
+            }
+
+            public void setGradeId(int gradeId) {
+                this.gradeId = gradeId;
+            }
+
+            public int getSubjectId() {
+                return subjectId;
+            }
+
+            public void setSubjectId(int subjectId) {
+                this.subjectId = subjectId;
+            }
+
+            public int getMaterialId() {
+                return materialId;
+            }
+
+            public void setMaterialId(int materialId) {
+                this.materialId = materialId;
+            }
+
+            public String getMaterialName() {
+                return materialName;
+            }
+
+            public void setMaterialName(String materialName) {
+                this.materialName = materialName;
+            }
+
+            public int getVersionId() {
+                return versionId;
+            }
+
+            public void setVersionId(int versionId) {
+                this.versionId = versionId;
+            }
+
+            public Object getVersionName() {
+                return versionName;
+            }
+
+            public void setVersionName(Object versionName) {
+                this.versionName = versionName;
+            }
+
+            public int getIfFiveFour() {
+                return ifFiveFour;
+            }
+
+            public void setIfFiveFour(int ifFiveFour) {
+                this.ifFiveFour = ifFiveFour;
+            }
+
+            public Object getMaterialPosterImg() {
+                return materialPosterImg;
+            }
+
+            public void setMaterialPosterImg(Object materialPosterImg) {
+                this.materialPosterImg = materialPosterImg;
+            }
+
+            public Object getUseCount() {
+                return useCount;
+            }
+
+            public void setUseCount(Object useCount) {
+                this.useCount = useCount;
+            }
+
+            public Object getOrderIndex() {
+                return orderIndex;
+            }
+
+            public void setOrderIndex(Object orderIndex) {
+                this.orderIndex = orderIndex;
+            }
+        }
+    }
+}
