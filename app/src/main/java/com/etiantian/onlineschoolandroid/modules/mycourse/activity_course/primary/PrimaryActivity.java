6b68f2df49a65f1d657f0a052602a2e9162b7068
waitwalker <waitwalker@163.com>
2020-09-21 13:56:26 +0800

    小升初活动课基本搞定

--- app/src/main/java/com/etiantian/onlineschoolandroid/modules/mycourse/activity_course/primary/PrimaryActivity.java
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/mycourse/activity_course/primary/PrimaryActivity.java
@@ -2,13 +2,16 @@ package com.etiantian.onlineschoolandroid.modules.mycourse.activity_course.prima
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 
 import com.etiantian.onlineschoolandroid.R;
+import com.etiantian.onlineschoolandroid.api.NetworkManager;
 import com.etiantian.onlineschoolandroid.base.BaseActivity;
+import com.etiantian.onlineschoolandroid.modules.common_tools.CommonWebViewActivity;
 
 public class PrimaryActivity extends BaseActivity implements CompoundButton.OnClickListener {
 
@@ -30,6 +33,11 @@ public class PrimaryActivity extends BaseActivity implements CompoundButton.OnCl
         backButton = findViewById(R.id.actionbar_back_button);
         backButton.setOnClickListener(this);
         chinese_relative = findViewById(R.id.primary_subject_chinese);
+        chinese_relative.setOnClickListener(this);
+        math_relative = findViewById(R.id.primary_subject_math);
+        math_relative.setOnClickListener(this);
+        english_relative = findViewById(R.id.primary_subject_english);
+        english_relative.setOnClickListener(this);
     }
 
     @Override
@@ -38,7 +46,23 @@ public class PrimaryActivity extends BaseActivity implements CompoundButton.OnCl
             case R.id.actionbar_back_button:
                 finish();
                 break;
-            case R.id.primary_activity_relative:
+            case R.id.primary_subject_chinese:
+                Intent intent1 = new Intent(this, CommonWebViewActivity.class);
+                intent1.putExtra("url", NetworkManager.HttpConstants.Primary_Activity_Chinese_URL);
+                intent1.putExtra("title","小升初暑期课程--语文");
+                startActivity(intent1);
+                break;
+            case R.id.primary_subject_math:
+                Intent intent2 = new Intent(this, CommonWebViewActivity.class);
+                intent2.putExtra("url", NetworkManager.HttpConstants.Primary_Activity_Math_URL);
+                intent2.putExtra("title","小升初暑期课程--数学");
+                startActivity(intent2);
+                break;
+            case R.id.primary_subject_english:
+                Intent intent3 = new Intent(this, CommonWebViewActivity.class);
+                intent3.putExtra("url", NetworkManager.HttpConstants.Primary_Activity_English_URL);
+                intent3.putExtra("title","小升初暑期课程--英语");
+                startActivity(intent3);
                 break;
         }
     }
