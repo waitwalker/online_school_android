c89bdbb0da0ecfd6b3ff66d25bca8481a543d591
waitwalker <waitwalker@163.com>
2020-09-24 14:08:38 +0800

    大师直播回放列表model添加

---
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/mycourse/live/LiveListGridView.java
@@ -0,0 +1,23 @@
+package com.etiantian.onlineschoolandroid.modules.mycourse.live;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+public class LiveListGridView extends GridView {
+    public LiveListGridView(Context context) {
+        super(context);
+    }
+
+    public LiveListGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LiveListGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public LiveListGridView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
