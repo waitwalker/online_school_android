36382672cd893c6069a14143a8ef59e2922ed2cb
waitwalker <waitwalker@163.com>
2020-10-10 10:28:35 +0800

    播放器添加完成

---
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/common_tools/OnTransitionListener.java
@@ -0,0 +1,35 @@
+package com.etiantian.onlineschoolandroid.modules.common_tools;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.transition.Transition;
+
+@TargetApi(Build.VERSION_CODES.KITKAT)
+public class OnTransitionListener implements Transition.TransitionListener {
+
+
+    @Override
+    public void onTransitionStart(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionEnd(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionCancel(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionPause(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionResume(Transition transition) {
+
+    }
+}
---
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/common_tools/SwitchVideoModel.java
@@ -0,0 +1,39 @@
+package com.etiantian.onlineschoolandroid.modules.common_tools;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by shuyu on 2016/12/7.
+ */
+
+public class SwitchVideoModel {
+    private String url;
+    private String name;
+
+    public SwitchVideoModel(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
