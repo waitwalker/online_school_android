5cff7e5e83849483197872b4714cc7625b03cf9a
waitwalker <waitwalker@163.com>
2020-10-12 15:24:09 +0800

    添加pdf预览容器activity

---
+++ app/src/main/java/com/etiantian/onlineschoolandroid/tools/PDFReaderActivity.java
@@ -0,0 +1,16 @@
+package com.etiantian.onlineschoolandroid.tools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.etiantian.onlineschoolandroid.R;
+
+public class PDFReaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_reader);
+    }
+}
