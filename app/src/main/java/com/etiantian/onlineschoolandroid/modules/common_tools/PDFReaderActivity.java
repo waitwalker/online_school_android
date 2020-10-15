da6a119debfea90003088d17c3de084e1d2ce75f
waitwalker <waitwalker@163.com>
2020-10-15 11:44:20 +0800

    调通pdf加载

--- app/src/main/java/com/etiantian/onlineschoolandroid/modules/common_tools/PDFReaderActivity.java
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/common_tools/PDFReaderActivity.java
@@ -3,39 +3,48 @@ package com.etiantian.onlineschoolandroid.modules.common_tools;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import com.etiantian.onlineschoolandroid.R;
+import com.etiantian.onlineschoolandroid.base.BaseActivity;
+import com.etiantian.onlineschoolandroid.modules.mycourse.ai_test.AITestListActivity;
+import com.etiantian.onlineschoolandroid.modules.mycourse.change_material.ChangeMaterialVersionActivity;
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
 import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
 import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
-import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.jaeger.library.StatusBarUtil;
 import com.liulishuo.filedownloader.BaseDownloadTask;
 import com.liulishuo.filedownloader.FileDownloadSampleListener;
 import com.liulishuo.filedownloader.FileDownloader;
 import com.liulishuo.filedownloader.util.FileDownloadUtils;
 import com.shockwave.pdfium.PdfDocument;
+import com.wuhenzhizao.titlebar.widget.CommonTitleBar;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
 
-public class PDFReaderActivity extends AppCompatActivity implements OnPageChangeListener, OnLoadCompleteListener,
-        OnPageErrorListener {
+public class PDFReaderActivity extends BaseActivity implements OnPageChangeListener, OnLoadCompleteListener,
+        OnPageErrorListener, View.OnClickListener {
 
     private PDFView pdfView;
+    private ViewGroup backButton;
+    private ViewGroup navigationBar;
+    private TextView back_button;
+    private CommonTitleBar commonTitleBar;
+    private Button knowledgeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_reader);
+        initActionBar();
 
         initView();
         Intent intent = getIntent();
@@ -47,18 +56,57 @@ public class PDFReaderActivity extends AppCompatActivity implements OnPageChange
         }
     }
 
+    ///
+    /// @description actionBar
+    /// @param
+    /// @return
+    /// @author waitwalker
+    /// @time 2020/9/29 9:09 AM
+    ///
+    private void initActionBar() {
+        hideActionBar();
+        navigationBar = findViewById(R.id.navigation_bar);
+        navigationBar.setBackgroundColor(Color.parseColor("#5FACEF"));
+        commonTitleBar = findViewById(R.id.actionbar);
+        commonTitleBar.setBackgroundColor(Color.parseColor("#5FACEF"));
+        StatusBarUtil.setColor(PDFReaderActivity.this, Color.parseColor("#5FACEF"));
+
+        backButton = findViewById(R.id.back_container);
+        backButton.setOnClickListener(this);
+        back_button = findViewById(R.id.action_bar_title);
+
+        back_button.setTextColor(Color.WHITE);
+        back_button.setOnClickListener(this);
+
+        knowledgeButton = findViewById(R.id.right_button);
+        knowledgeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back_container:
+            case R.id.action_bar_title:
+                finish();
+                break;
+        }
+    }
+
     //下载具体操作
     private void download(String pafURL, String title) {
+        back_button.setText(title);
         try {
 
             //final String filePath = getExternalFilesDir("").getPath() + File.separator + "pdf" + File.separator + title + ".pdf";
 
             final String filePath = FileDownloadUtils.getDefaultSaveRootPath() + File.separator + "pdf" + File.separator + title + ".pdf";
 
+            hud.show();
             File file1 = new File(filePath);
             if (file1.exists()) {
                 loadPDF(filePath);
             } else {
+
                 BaseDownloadTask task = FileDownloader.getImpl().create(pafURL)
                         .setPath(filePath, false)
                         .setCallbackProgressTimes(300)
@@ -116,6 +164,7 @@ public class PDFReaderActivity extends AppCompatActivity implements OnPageChange
                 .enableDoubletap(true)
                 .defaultPage(0)
                 .load();
+        hud.dismiss();
     }
 
 
