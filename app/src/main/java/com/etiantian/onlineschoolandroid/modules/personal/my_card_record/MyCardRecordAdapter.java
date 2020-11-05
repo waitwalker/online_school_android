cbe9d33eca49e53b008df38f9472424c1d16b9d1
waitwalker <waitwalker@163.com>
2020-11-05 14:05:01 +0800

    我的卡记录适配器添加调整

--- app/src/main/java/com/etiantian/onlineschoolandroid/modules/personal/my_card_record/MyCardRecordAdapter.java
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/personal/my_card_record/MyCardRecordAdapter.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.etiantian.onlineschoolandroid.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,7 +38,20 @@ public class MyCardRecordAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        ViewHolder viewHolder;
+        MyCardRecordModel.DataBean dataBean = dataSources.get(i);
+        if (view == null) {
+            viewHolder = new ViewHolder();
+            view = View.inflate(context, R.layout.my_card_record_layout, null);
+            viewHolder.cardTitleTextView = view.findViewById(R.id.my_card_title);
+            viewHolder.timeTextView = view.findViewById(R.id.my_card_time);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.cardTitleTextView.setText(dataBean.getCourseCardName());
+        viewHolder.timeTextView.setText(dataBean.getActivationTime());
+        return view;
     }
 
     private class ViewHolder {
