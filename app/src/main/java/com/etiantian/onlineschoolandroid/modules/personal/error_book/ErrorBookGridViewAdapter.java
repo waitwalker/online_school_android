56de85c4328853d2c83eaa9d650b792c5bb59804
waitwalker <waitwalker@163.com>
2020-11-18 08:58:24 +0800

    错题本grid view adapter 添加

---
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/personal/error_book/ErrorBookGridViewAdapter.java
@@ -0,0 +1,27 @@
+package com.etiantian.onlineschoolandroid.modules.personal.error_book;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ErrorBookGridViewAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
