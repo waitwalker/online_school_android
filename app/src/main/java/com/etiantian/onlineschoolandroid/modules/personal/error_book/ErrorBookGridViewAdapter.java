44475d998b446fdfb5aed84cdb71a3c7478c93b0
waitwalker <waitwalker@163.com>
2020-11-19 11:38:59 +0800

    错题本adapter设置

--- app/src/main/java/com/etiantian/onlineschoolandroid/modules/personal/error_book/ErrorBookGridViewAdapter.java
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/personal/error_book/ErrorBookGridViewAdapter.java
@@ -1,27 +1,54 @@
 package com.etiantian.onlineschoolandroid.modules.personal.error_book;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.etiantian.onlineschoolandroid.R;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class ErrorBookGridViewAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<Map> dataSource;
+
+    public ErrorBookGridViewAdapter(Context context, ArrayList<Map> dataSource) {
+        this.context = context;
+        this.dataSource = dataSource;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return dataSource.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return dataSource.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        ViewHolder viewHolder;
+        if (view == null) {
+            view = View.inflate(context, R.layout.error_book_entrance_card_layout, null);
+            viewHolder = new ViewHolder();
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        return view;
+    }
+
+    private class ViewHolder {
+
     }
 }
