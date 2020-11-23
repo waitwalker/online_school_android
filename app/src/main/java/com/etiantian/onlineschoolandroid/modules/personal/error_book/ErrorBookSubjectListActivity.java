38b4cec96d62c835bb0a9216dd331920bbd6b86f
waitwalker <waitwalker@163.com>
2020-11-23 16:10:17 +0800

    错题本学科列表添加

---
+++ app/src/main/java/com/etiantian/onlineschoolandroid/modules/personal/error_book/ErrorBookSubjectListActivity.java
@@ -0,0 +1,16 @@
+package com.etiantian.onlineschoolandroid.modules.personal.error_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.etiantian.onlineschoolandroid.R;
+
+public class ErrorBookSubjectListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error_book_subject_list);
+    }
+}
